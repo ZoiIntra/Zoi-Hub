4f0676fa8bcc000e13c95276a4b2ec4ccd7b48f5
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2022-04-12 12:34:04 +0700

    Update WhiteList.lua

--- Main/WhiteList.lua
+++ Main/WhiteList.lua
@@ -2,9 +2,16 @@ local Z0i = { 000, 001, 002 }
 genv().ZoiCs3179 = Z0i
 
 -- ofc.
+wait(1)
 local player = game.Players.LocalPlayer
 for i,v in pairs(genv().ZoiCs3179) do
 	if v == player.UserId then
-		print()
+		print("ZoiHub has start loading!")
+		wait(1)
+		print("Wait for Gui...")
+		--script
+		print("Done!")
+	else
+		player:Kick("Incorrect Key!")
 	end
 end
