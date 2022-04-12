f9e9c6d35938b86e4beecec8b8c3bf7468791b22
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2022-04-12 13:17:26 +0700

    Update WhiteList.lua

--- Main/WhiteList.lua
+++ Main/WhiteList.lua
@@ -4,7 +4,7 @@ genv().ZoiCs3179 = Z0i
 -- ofc.
 wait(1)
 local time = 18*100
-local loop09 = 0
+local loop09 = false
 local count = 30
 local player = game.Players.LocalPlayer
 local RealUserId = genv().ZoiCs3179/97
@@ -12,15 +12,17 @@ for i,v in pairs(RealUserId) do
 	if v == player.UserId then
 		print("Done!")
 	else
-		loop09 = 1
+		loop09 = true
 		wait(time)
 		player:Kick("You use script for free, You can use only 30min!")
 	end
 end
 
 -- end script
-while loop09 > 0 do
-	print("Time for use script : "..count.."min.")
-	count = count - 1
-	wait(60)
+if loop09 == true then
+	while count > 0 do
+		print("Time for use script : "..count.."min.")
+		count = count - 1
+		wait(60)
+	end
 end
